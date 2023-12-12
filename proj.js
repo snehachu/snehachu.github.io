e1f4b661bb1cd1164989a98d9fa3371c1e6aeb5f
snehachu <153590380+snehachu@users.noreply.github.com>
2023-12-12 21:04:53 +1100

    Add files via upload

---
+++ proj.js
@@ -0,0 +1,11 @@
+
+
+
+function openCustomModal() {
+    document.getElementById("custom-modal").style.display = "flex";
+}
+
+function closeCustomModal() {
+    document.getElementById("custom-modal").style.display = "none";
+}
+
